00e6494303d09b2118ff8eda0bb6f2b727fba8a1
Shyam-1811 <shyamverma1811@gmail.com>
2024-07-14 23:51:37 +0530

    first commit

---
+++ Day_2.js
@@ -0,0 +1,69 @@
+// Day - 2
+// Arithmetic Operation
+// Task - 1
+let Num1 = 20;
+let Num2 = 10;
+console.log(`The Sum is : ${Num1 + Num2}`);
+// Task - 2
+console.log(`The Difference is : ${Num1 - Num2}`);
+// Task - 3
+console.log(`The product is : ${Num1 * Num2}`);
+// Task - 4
+console.log(`The division is : ${Num1 / Num2}`);
+// Task - 5
+console.log(`The remainder is : ${Num1 % Num2}`);
+// Assignment operator
+// task- 6
+let num3 = 20
+num3 += 30;
+console.log(`The result is : ${num3}`);
+//  Task - 7
+num3 -= 10;
+console.log(`The difference is : ${num3}`);
+// Comparison operators
+// Task - 8
+let num4 = 33;
+let num5 = 22;
+if (num4 > num5) {
+    console.log(`num4 is greater than num5`);
+}
+if (num5 < num4) {
+    console.log(`num5 is greater than num4`);
+}
+// Task - 9
+if (num4 >= num5) {
+    if (num4 > num5) {
+        console.log(`num4 is greater than num5`);
+    }
+}
+// Task - 10
+let num6 = 100
+let num7 = "100";
+if (num6 == num7) {
+    console.log(`num6 is equal to num7`)
+}
+if (num6 === num7) {
+    console.log(`both are equal`)
+}
+else
+{
+    console.log(`num6 is not equal to num7`)
+}
+// Task = 11
+if (num6 >50 && num6 >60) {
+    console.log(`It uses and operator`)
+}
+// Task = 12 
+if (num6 > 50 || num6 >60) {
+    console.log(`It uses or operator`)
+}
+// Task = 13
+if (num6 != num5) {
+    console.log(`Use of ! operator`)
+}
+// Ternary Operator
+num6 > 0 ? console.log(true): console.log(false)
+
+
+
+
